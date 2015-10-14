8d944719b57ab76e31595c21fe7daaed0ad18dcb
ysakamoto <yus.sakamoto@gmail.com>
2015-10-14 09:02:13 -0500

    functionalization of script complete

--- sccomp.py
+++ sccomp.py
@@ -1,4 +1,5 @@
 import os
+from os.path import join as opj
 from pdb import set_trace as st
 
 import numpy as np
@@ -13,15 +14,6 @@ import mask_tk as mtk
 
 from pb import poisson_blend, create_mask
 
-
-gists_db_name = 'dbs/gist_data.npy'
-f_db_name = 'dbs/file_names.npy'
-
-data_dir = 'data/scene_database/'
-img_dirs = [d for d in os.listdir(data_dir)
-            if os.path.isdir(os.path.join(data_dir, d))]
-
-mask_name = 'mask.png'
 
 # param = param_gist()
 # param.img_size = 256
@@ -64,18 +56,18 @@ def create_gist_database(data_dir, img_dirs):
     return
 
 
-def load_gists(gist_db_name, f_db_name):
+def load_gists(gists_db_name, f_db_name, param_name):
     # load saved GIST features
     gist_data = np.load(gists_db_name)
     file_names = np.load(f_db_name)
     gist_data = gist_data.reshape((2688, 512)).T
-    with open('gistparams', 'r') as f:
+    with open(param_name, 'r') as f:
         param = pickle.load(f)
 
     return gist_data, file_names, param
 
 
-def load_queries(img_dirs, data_dir, num_img, mask_name):
+def load_queries(img_dirs, data_dir, num_img, mask_name, query_name=None):
     """
     Load query image
     """
@@ -86,10 +78,17 @@ def load_queries(img_dirs, data_dir, num_img, mask_name):
         img_files += [os.path.join(scene_dir, f) for f in os.listdir(scene_dir)
                       if os.path.isfile(os.path.join(scene_dir, f))
                       and 'jpg' in f]
-    try:
-        query_name = img_files[num_img]
-    except IndexError:
-        print "choose from 0 ~ %d!" % len(img_files)
+
+    if query_name is not None:
+        for imf in img_files:
+            if query_name in imf:
+                query_name = imf
+                break
+    else:
+        try:
+            query_name = img_files[num_img]
+        except IndexError:
+            print "choose from 0 ~ %d!" % len(img_files)
 
     img_query = io.imread(query_name)
 
@@ -134,10 +133,14 @@ def get_gist(query_name, img_query, img_mask, param):
     return gist, block_weight
 
 
-def get_matches(gist_data, file_names, img_mask):
+def get_matches(gist, gist_data, block_weight, file_names, img_mask,
+                query_name, mask_name, plot_figure=True,
+                save_dir=None):
     """
     Using the gist value, get the closes matches from the database
     """
+
+    offset = (0, 0)
 
     # find the close matches of a scene chosen using the
     er = np.linalg.norm(
@@ -145,22 +148,34 @@ def get_matches(gist_data, file_names, img_mask):
     agst = np.argsort(er)
 
     file_names = np.array(file_names)
-    fn_sorted = file_names[agst][1:6]
+    fn_sorted = file_names[agst][1:7]
     print '\nImages chosen from scene completion are:\n', fn_sorted
 
     img_target = io.imread(query_name).astype(np.float64)
-    img_sc = poisson_blend(img_mask, img_mask, img_target,
-                           method='src', offset_adj=(0, 0))
-    print '\nOriginal image with mask.'
-    plt.imshow(img_sc)
-    plt.show()
 
+    img_mask, _, offset_adj \
+        = create_mask(img_mask.astype(np.float64),
+                      img_target, img_mask, offset=offset)
+    img_sc = poisson_blend(img_mask, 1-img_mask, img_target,
+                           method='src', offset_adj=(0, 0))
+
+    mst_name =  query_name.split('/')[-1].split('.jpg')[0]
+    mst_name = opj(save_dir, mst_name+'mask.jpg')
+    
+    if plot_figure:
+        print '\nOriginal image with mask.'
+        plt.imshow(img_sc)
+        plt.show()
+    if save_dir is not None:
+        io.imsave(mst_name, img_sc)
+        
     print '\nCompleted images from 1st to 5th choices:'
+    sc_names = []
     for i in range(len(fn_sorted)):
         # img_target = io.imread(query_name).astype(np.float64)
+        print fn_sorted[i]
         img_src = io.imread(fn_sorted[i]).astype(np.float64)
-        img_mask = io.imread(mask_name, as_grey=True)
-        offset = (0, 0)
+        # img_mask = io.imread(mask_name, as_grey=True)
 
         img_mask, img_src, offset_adj \
             = create_mask(img_mask.astype(np.float64),
@@ -168,34 +183,59 @@ def get_matches(gist_data, file_names, img_mask):
 
         img_sc = poisson_blend(img_mask, img_src, img_target, method='normal',
                                offset_adj=offset)
-        plt.imshow(img_sc)
-        plt.show()
 
-    return fn_sorted
+        if plot_figure:
+            plt.imshow(img_sc)
+            plt.show()
+
+        if save_dir is not None:
+            sc_name = '%s+%s.jpg' %(
+                query_name.split('/')[-1].split('.jpg')[0],
+                fn_sorted[i].split('/')[-1].split('.jpg')[0])
+            sc_name = opj(save_dir, sc_name)
+            io.imsave(sc_name, img_sc)
+            sc_names.append(sc_name)
+            
+    return mst_name, fn_sorted, sc_names
 
 
-create_gist_database(data_dir, img_dirs)
 
-gist_data, file_names, param = load_gists(gists_db_name, f_db_name)
+if __name__ == "__main__":
 
-# ask for the image number to work with
-print "Choose the image number"
-num_img = int(raw_input())
+    gists_db_name = 'dbs/gist_data.npy'
+    f_db_name = 'dbs/file_names.npy'
 
-query_name, img_query, img_files \
-    = load_queries(img_dirs, data_dir, num_img, mask_name)
+    data_dir = 'data/scene_database/'
+    img_dirs = [d for d in os.listdir(data_dir)
+                if os.path.isdir(os.path.join(data_dir, d))]
 
-uin = 'Y'
-if os.path.exists(mask_name):
-    print "Mask file already exist. Create a new one? (Y|n)"
-    uin = raw_input()
-if uin in ['Y', '']:
-    mtk.create_mask(query_name, mask_name)
+    mask_name = 'mask.png'
+    param_name = 'gistparams'
 
-img_mask = io.imread(mask_name, as_grey=True)
+    create_gist_database(data_dir, img_dirs)
+    
+    gist_data, file_names, param = load_gists(gists_db_name, f_db_name, param_name)
 
-gist, block_weight = get_gist(query_name, img_query, img_mask, param)
+    # ask for the image number to work with
+    print "Choose the image number"
+    num_img = int(raw_input())
 
-matches = get_matches(gist_data, file_names, img_mask)
+    query_name, img_query, img_files \
+        = load_queries(img_dirs, data_dir, num_img, mask_name)
 
-# show_gist(gd, param)
+    uin = 'Y'
+    if os.path.exists(mask_name):
+        print "Mask file already exist. Create a new one? (Y|n)"
+        uin = raw_input()
+    if uin in ['Y', '']:
+        mtk.create_mask(query_name, mask_name)
+
+    img_mask = io.imread(mask_name, as_grey=True)
+
+    gist, block_weight = get_gist(query_name, img_query, img_mask, param)
+
+    mst_name, sc_names, matches = get_matches(gist, gist_data, block_weight,
+                                              file_names, img_mask, query_name,
+                                              mask_name, save_dir='')
+
+    # show_gist(gd, param)
